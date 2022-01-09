72b3006041ea1c3d341b0b9245c503788302e5e4
Natali-QA-ooops <80644500+Natali-QA-ooops@users.noreply.github.com>
2022-01-09 21:45:02 +0200

    Tables xPath

--- src/test/java/TablesCss.java
+++ src/test/java/TablesCss.java
@@ -18,28 +18,35 @@ public class TablesCss {
     public void tableTest(){
         //найти таблицу
         wd.findElement(By.cssSelector("table"));
-
+        wd.findElement(By.xpath("//table"));
 
         // tr9 - one element
         wd.findElement(By.cssSelector("tr:last-child"));
+        wd.findElement(By.xpath("//tr[last()]"));
 
         //Maria Anders - one element
         wd.findElement(By.cssSelector("tr:nth-child(2) td:nth-child(2)"));
+        wd.findElement(By.xpath("//tr[2]//td[2]"));
 
         // last column  -all elements
         wd.findElements(By.cssSelector("td:last-child"));
+        wd.findElements(By.xpath("//td[last()]"));
 
         // th elements
         wd.findElements(By.cssSelector("th"));
+        wd.findElements(By.xpath("//th"));
 
         //Centro comercial Moctezuma	Francisco Chang	Mexico -  one element
         wd.findElement(By.cssSelector("tr:nth-child(4)"));
+        wd.findElement(By.xpath("//tr[4]"));
 
         //	Giovanni Rovelli  = one element
         wd.findElement(By.cssSelector("tr:last-child td:nth-child(2)"));
+        wd.findElement(By.xpath("//tr[last()]//td[2]"));
 
         // найти все элементы первой колонки -   // first column - all element
         wd.findElements(By.cssSelector("td:first-child"));
+        wd.findElements(By.xpath("//td[1]"));
 
     }
 }
